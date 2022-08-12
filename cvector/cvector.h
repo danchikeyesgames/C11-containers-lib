ddc0aa23b06bd72221ac4a06ec6dc7561583597a
danchikeyesgames <danchikhol@gmail.com>
2022-08-12 17:17:34 +0300

    add init_function for cvector

--- cvector/cvector.h
+++ cvector/cvector.h
@@ -16,20 +16,31 @@ typedef struct vec_cvector_struct {
     void*  data;
 } cvector_t;
 
+/*********************
+segment with define data
+*********************/
+
+/* gets the current capacity of the vector */ 
 #define cvector_get_capacity(v)  \
-        (v) ? ((size_t *) (v))[-2] : (size_t)0;
+        (v) ? ((size_t *) (v))[-2] : (size_t)0
 
+/* gets the current size of the vector */
 #define cvector_get_size(v)  \
-        (v) ? ((size_t *) (v))[-1] : (size_t)0;
+        (v) ? ((size_t *) (v))[-1] : (size_t)0
 
+/* macros gets destructor of the vector (need for others functions) */
 #define cvector_get_destructor(v)  \
-        (v) ? (((cvector_destructor_elem_t *) (&(((size_t *) (v))[-2])))[-1]) : NULL;
+        (v) ? (((cvector_destructor_elem_t *) (&(((size_t *) (v))[-2])))[-1]) : NULL
 
+/* macros gets constructor of the vector (need for others functions) */
 #define cvector_get_constructor(v) \
-        (v) ? (((cvector_constructor_elem_t *) (&(((cvector_destructor_elem_t *) (&(((size_t *)) (v))[-2])))[-1]))[-1]) : NULL;  
+        (v) ? (((cvector_constructor_elem_t *) (&(((cvector_destructor_elem_t *) (&(((size_t *)) (v))[-2])))[-1]))[-1]) : NULL
 
+/* checks if vector is empty
+@@@ return 1 - if the vector is empty, other case - 0;
+*/
 #define cvector_empty(v)                                    \
-        (cvector_size) ? 0 : 1
+        (cvector_size(v)) ? 0 : 1
 
 #define cvector_full(v)                                     \
         ((cvector_get_size) == (cvector_get_capacity)) ? 1 : 0
@@ -40,27 +51,31 @@ typedef struct vec_cvector_struct {
         do {                                                \
             if (!(v))                                       \
                 cvector_set_grow(v, 0);                     \
-            (cvector_get_constructor) = construct_fun;      \
+            (((cvector_constructor_elem_t *) (&(((cvector_destructor_elem_t *) (&(((size_t *) (v))[-2])))[-1])))[-1]) = construct_fun;   \
         } while(0)
 
-#define cvector_set_festructor(v, destructor_fun)           \
+#define cvector_set_destructor(v, destructor_fun)           \
         do {                                                \
             if (!(v))                                       \
                 cvector_set_grow(v, 0);                     \
-            (cvector_get_destructor) = destructor_fun;      \
+            (((cvector_destructor_elem_t *) (&(((size_t *) (v))[-2])))[-1]) = destructor_fun;   \
         } while(0)
 
 #define cvector_set_size(v, sz)                             \
         if ((v))                                            \
-            (cvector_get_size) = sz
+            ((size_t *) (v))[-1] = sz
 
 #define cvector_set_capacity(v, sz)                         \
-        if (v)                                              \
-            (cvector_get_capacity = sz
+        if ((v))                                            \
+            ((size_t *) (v))[-2] = sz
                     
+#define cvector_push_back(vec, object) __cvector_push_back((void *) &vec, (void *) &object, sizeof(object))
+
 /* user interface functions */
 
-void cvector_push_back();
+void*   __cvector_initialization_type(int size, size_t size_object);
+void    __cvector_push_back(void* vec, void* object, size_t size_object);
+void    cvector_set_grow(void *v, size_t size);
 
 #endif  // __CVECTOR_H__
 
