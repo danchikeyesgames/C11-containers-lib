a52882e540f3568d1d9f20b6244fdf3cbaa40a3d
danchikeyesgames <danchikhol@gmail.com>
2022-09-05 20:21:38 +0300

    release pop&push functions

--- cstack/cstack.c
+++ cstack/cstack.c
@@ -1,5 +1,6 @@
 #include <cstack.h>
 #include <stdlib.h>
+#include <string.h>
 
 void cstack_createStack(cstack_header* header) {
     *header = cstack_malloc();
@@ -7,20 +8,64 @@ void cstack_createStack(cstack_header* header) {
     (*header)->head = NULL;
 }
 
+void cstack_push(cstack_header header, void* data, size_t size_object) {
+    cstack *new_node = cstack_malloc_node();
+    Item* new_Item = cstack_malloc_Item(size_object, data);
+    cstack* prevNode = header->head;
 
+    new_node->data  = new_Item;
+    new_node->next  = prevNode;
+    header->head    = new_node;
+}
+
+void* cstack_pop_back(cstack_header header) {
+    cstack* node = header->head;
+    void* p = NULL;
+
+    if (node) {
+        p = node->data;
+        header->head = node->next;
+        free(node);
+    }
+
+    return p;
+}
+
+void cstack_destroy_each_node(cstack_header* header) {
+    cstack* node = (*header)->head;
+    
+    while (node) {
+        node = node->next;
+        cstack_free_node(node);
+    }
+
+    *header = NULL;
+}
 
 /*---------------------------------
           static functions
 ---------------------------------*/
 
-static Item* cstack_malloc_Item() {
+static Item* cstack_malloc_Item(size_t sz, void* data) {
+    Item* node = (Item *) malloc(sizeof(Item));
+    void* new_data = malloc(sz);
 
+    memcpy(new_data, data, sz);
+    node->size_object = sz;
+    node->data = new_data;
+
+    return node;
 }
 
-static cstack* cstack_malloc_node() {
-
+static cstack* cstack_malloc_node(void) {
+    return (cstack *) malloc(sizeof(struct __cstack));
 }
 
 static cstack_header cstack_malloc(void) {
     return (cstack_header) malloc(sizeof(struct __header));
+}
+
+static void cstack_free_node(cstack* node) {
+    free(node->data);
+    free(node);
 }
--- cstack/cstack.h
+++ cstack/cstack.h
@@ -6,7 +6,7 @@
 #define CSTACK_STRUCT_ENABLE
 #ifdef CSTACK_STRUCT_ENABLE
 
-typedef struct Item_sctack {
+typedef struct Item_cstack {
     size_t size_object;
     void* data;
 } Item;
@@ -15,7 +15,7 @@ typedef struct __cstack {
     Item* data;
     struct __cstack* next;
 } cstack;
-#endif // CSTACK_STRUCT_ENABLE
+#endif      // CSTACK_STRUCT_ENABLE
 
 typedef struct __header {
     size_t size;
@@ -25,6 +25,16 @@ typedef struct __header {
 #define cstack_isEmpty(head) (!head->size)
 
 #define cstack_push_back(header, data) cstack_push(header, data, data ? sizeof(*data) : NULL)
+
+#define cstack_unpack(header) header->head->data
+
+#define cstack_pop_back_type(header, TYPE) header->head ? *((TYPE *) cstack_unpack(header)) : 0;    \
+        do {                                                \
+        cstack* new = header->head->next;                   \
+        free(header->head->data);                           \
+        free(header->head);                                 \
+        header->head = new;                                 \
+        } while(0)
 
 #define cstack_create_stack(header) cstack_createStack(&header)
 
@@ -39,4 +49,4 @@ void*   cstack_pop_back(cstack_header header);
 void    cstack_destroy_each_node(cstack_header* header);
 
 #undef CSTACK_STRUCT_ENABLE
-#endif    // __CSTACK_H__
+#endif      // __CSTACK_H__
