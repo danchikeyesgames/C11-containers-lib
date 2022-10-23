314d8f3b1a8f455226efcc1d5a2c8702a7292ef7
danchikeyesgames <danchikhol@gmail.com>
2022-10-23 23:48:10 +0300

    add comments in cstack.h

--- cstack/cstack.h
+++ cstack/cstack.h
@@ -20,6 +20,11 @@ typedef struct __cstack_node {
     struct  __cstack_node* next;
 } cstack_node;
 
+/**
+ * @brief header structure for cstack
+ * 
+ * @struct cstack_t
+ */
 typedef struct __header {
     size_t          bytes;
     size_t          size;
@@ -28,10 +33,29 @@ typedef struct __header {
     cstack_constructor_t constructor;
 } *cstack_t;
 
+/**
+ * @brief gets current size of the stack 
+ * 
+ * @param cstack your stack
+ */
 #define cstack_get_size(cstack) _sf_cstack_get_size(cstack)
 
+/**
+ * @brief checks empty of stack 
+ * 
+ * @param cstack your stack
+ * 
+ * @return 1: if stack empty, 0: if size > 0
+ */
 #define cstack_empty(cstack) _sf_cstack_empty(cstack)
 
+/**
+ * @brief gets top stack node
+ * 
+ * @param header your stack
+ * 
+ * @return pointer on top element of the stack
+ */
 #define cstack_unpack(header) (header->head)
 
 #define cstack_create(cstack_header, f_const, f_dest, TYPE) _sf_cstack_create(&cstack_header, f_const, f_dest, sizeof(TYPE))
@@ -40,9 +64,9 @@ typedef struct __header {
 
 #define cstack_get_data_ptr(header) (_sf_cstack_get_data_ptr(header))
 
-/**************************************************
-* * * * * * * * functions segment * * * * * * * * *
-***************************************************/
+/***************************************************
+ * * * * * * * * functions segment * * * * * * * * *
+ ***************************************************/
 
 void    _sf_cstack_create(cstack_t* header, cstack_constructor_t c, cstack_destructor_t d, size_t type_size);
 
